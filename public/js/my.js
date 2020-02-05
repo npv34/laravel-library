1f1617310cd1e158396694bd057c9fe4a2f999a5
hang198 <hang17111998@gmail.com>
2020-02-05 17:26:42 +0700

    select-customer-and-book-to-borrow-with-ajax

--- public/js/my.js
+++ public/js/my.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
                     html +=   value['class'];
                     html += '</td>';
                     html += '</tr>';
-                })
+                });
 
                 $('#customer-table').html(html)
             }
@@ -46,11 +46,59 @@ $(document).ready(function () {
                     html += '<td>';
                     html +=   value['status'];
                     html += '</td>';
+                    html += '<td>';
+                    html +=   '<button data-id="'+value['id']+'"  class="btn btn-primary select-book">';
+                    html += 'Chon'
+                    html +=   '</button>';
+                    html += '</td>';
                     html += '</tr>';
-                })
-
+                });
                 $('#book-table').html(html)
             }
         });
     });
+
+   $('.select-customer').click(function () {
+        let idCustomer = $(this).attr("data-id");
+       $.ajax({
+           url: 'http://localhost:8000/admin/customers/' + idCustomer ,
+           type: 'GET',
+           success: function (result) {
+               //dong model lai
+                $('.modal-right').modal('hide');
+               //gan gia tri tra ve vao form
+               $('#name-customer').val(result['name']);
+               $('#code-customer').val(result['codeID']);
+               $('#class-customer').val(result['class']);
+               $('#dob-customer').val(result['dob']);
+
+           }
+       });
+   });
+
+
+   $('body').on('click','.select-book', function () {
+       let idBook = $(this).attr("data-id");
+       $.ajax({
+           url: 'http://localhost:8000/admin/books/' + idBook,
+           type: 'GET',
+           success: function (result) {
+               let status = (result['status'] === 1) ? 'Mới' : 'Cũ';
+
+
+               $('.modal-right').modal('hide');
+               let html = '';
+               html += '<tr>';
+               html += '<td>';
+               html +=   result['name'];
+               html += '</td>';
+               html += '<td>';
+               html +=   status;
+               html += '</td>';
+               html += '</tr>';
+
+               $('#book-table-choose').html(html)
+           }
+       });
+   });
 });
